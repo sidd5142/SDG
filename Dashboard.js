5f6b8105ccc768a704b9f1cd45a6d56ceea4d7bc
sidd5142 <siddharthyadav5142@gmail.com>
2023-11-29 01:27:46 +0530

    commit

--- Dashboard.js
+++ Dashboard.js
@@ -1,2 +1,52 @@
-app.controller("DashboardController", function ($scope, $http, $window, $state) {
-})
+app.controller(
+    "DashboardController",
+    function ($scope, $http, $window, $state) {
+      $http
+        .get("https://bf07-103-72-6-89.ngrok-free.app/UnityGoals/verify_ngo", {
+          headers: {
+            Accept: "application/json",
+            "ngrok-skip-browser-warning": "true",
+          },
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      [];
+      $scope.add = function () {
+        console.log($scope.files);
+        $http
+          .post(
+            "https://bf07-103-72-6-89.ngrok-free.app/UnityGoals/verify_ngo",
+            $scope.files
+          )
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      };
+    }
+  );
+  app.directive("fileModel", [
+    "$parse",
+    function ($parse) {
+      return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+          var model = $parse(attrs.fileModel);
+          var modelSetter = model.assign;
+  
+          element.bind("change", function () {
+            scope.$apply(function () {
+              var file = element[0].files[0];
+              modelSetter(scope, file);
+            });
+          });
+        },
+      };
+    },
+  ]);
--- routing.js
+++ routing.js
@@ -1,32 +1,35 @@
-var app = angular.module("myApp", ['ui.router']);
+var app = angular.module("myApp", ["ui.router"]);
 
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) { 
-	$stateProvider
-	    .state('Registration', {
-            url: '/register',
-            templateUrl: '/Home/registration.html',
-            controller: 'RegistrationController'
-        }) 
-        .state('SignUp', {
-            url: '/login',
-            templateUrl: '/Home/login.html',
-            controller: 'SignUpController'
-        }) 
-        .state('volunteer', {
-            url: '/volunteer',
-            templateUrl: '/Home/volunteer.html',
-            controller: 'VolunteerController'
-        }) 
-        .state('Dashboard', {
-            url: '/dashboard',
-            templateUrl: 'Dashboard.html',
-            controller: 'DashboardController'
-        }) 
-        .state('Dashboard.Verification', {
-            url: '/verification',
-            templateUrl: '/Pages/Verification.html',
-            controller: 'VerifyController'
-        }) 
-        $urlRouterProvider.otherwise('/login');
-
-}]) ;
+app.config([
+  "$stateProvider",
+  "$urlRouterProvider",
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("Registration", {
+        url: "/register",
+        templateUrl: "/Home/registration.html",
+        controller: "RegistrationController",
+      })
+      .state("SignUp", {
+        url: "/login",
+        templateUrl: "/Home/login.html",
+        controller: "SignUpController",
+      })
+      .state("volunteer", {
+        url: "/volunteer",
+        templateUrl: "/Home/volunteer.html",
+        controller: "VolunteerController",
+      })
+      .state("Dashboard", {
+        url: "/dashboard",
+        templateUrl: "Dashboard.html",
+        controller: "DashboardController",
+      })
+      .state("Dashboard.Verification", {
+        url: "/verification",
+        templateUrl: "/Pages/Verification.html",
+        controller: "VerifyController",
+      });
+    $urlRouterProvider.otherwise("/login");
+  },
+]);
